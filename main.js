c0d170faa6034ac4c62ec006b27e2dd89bf9e5f3
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2022-11-23 14:03:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function reset()
+{
+    
+}
+
+
+function fn1() {
+    if (document.getElementById("img1").src == "backCard.png"){
+        document.getElementById("img1").src = "2_of_diamonds.png";
+    } 
+    else {
+        document.getElementById("img1").src = "2_of_diamonds.png";
+    }  
+
+}
+
+function fn2()
+{
+    if (document.getElementById("img2").src == "backCard.png"){
+        document.getElementById("img2").src = "9_of_hearts.png";
+    } 
+    else {
+        document.getElementById("img2").src = "9_of_hearts.png";
+    }
+}
+
+function fn3()
+{
+    if (document.getElementById("img3").src == "backCard.png"){
+        document.getElementById("img3").src = "2_of_diamonds.png";
+    } 
+    else {
+        document.getElementById("img3").src = "2_of_diamonds.png";
+    }
+}
